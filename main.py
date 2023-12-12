e24b57f9124bf9cac3e78f6e18d4d9d6fef03a7b
rachit98 <rachity@gmail.com>
2023-12-12 18:30:33 +0530

    Health Checks

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import qrcode
 from io import BytesIO
 from base64 import b64encode
 from werkzeug.utils import url_quote
+import socket
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/', methods=['POST'])
 def generateQR():
@@ -21,6 +23,23 @@ def generateQR():
     base64_img = 'data:image/png;base64,' + b64encode(memory.getvalue()).decode('ascii')
 
     return render_template('index.html', data=base64_img)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(
+        status="UP"
+    )
+
+
+@app.route('/fetchdetails', methods=['GET'])
+def fetchdetails():
+    try:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        return render_template('details.html', hostname=hostname, ip=ip_address)
+    except:
+        return render_template('details.html', hostname='NULL', ip='NULL')
 
 
 app.run(host="0.0.0.0", port=5000)
